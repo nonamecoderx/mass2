a86d97689cc25d67f83a09b9e63d72f8b1476439
Иван <ivankiseliov@rambler.ru>
2022-09-27 21:45:52 +0300

    dz-4

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (100000 + (Math.random() * (200000 - 100000)));
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
-        String intAString = Arrays.toString(arr);
-        System.out.println(intAString);
-        double middle = 0;
-        double summ = 0;
-        for (int i = 0; i < arr.length; i++) {
-            summ += arr[i];
-        }
-        middle = summ / arr.length;
-        String result = String.format("%.2f", middle);
-        System.out.print("Средняя сумма трат за месяц составила "+ result +" рублей.");
     }
 }
 
